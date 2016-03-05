8aead0ab0dd816dc6ea1ba06d7fc0eccecdd252f
Nikola Kolevski <nikola.z.kolevski@gmail.com>
2016-03-05 05:34:35 +0100

    Add realtime posts entrypoint.

--- realtime/posts.js
+++ realtime/posts.js
@@ -3,5 +3,12 @@ var assert = require('assert');
 
 module.exports = function(io, app) {
     io.on('connection', function(socket) {
+        r.db(app.locals.config.rethinkdb.db_name).table('event').changes().run(app.locals.rdbConn, function(err, cursor) {
+            cursor.each(function(err, row) {
+                assert(err == null, err);
+
+                socket.emit('posts', row);
+            });
+        });
     });
 };
