43e2db99ef9843555a0e2fe1826c634af26efd8d
Nikola Kolevski <nikola.z.kolevski@gmail.com>
2016-03-05 11:23:39 +0100

    Add jade based database fetch.

--- routes/coordinators.js
+++ routes/coordinators.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var r = require('rethinkdb');
 
 /* GET coordinator page. */
 router.get('/dashboard/', function(req, res, next) {
-  res.render('coordinator-dashboard');
+  r.db.table('event').run(req.rdbConn, function(err, cursor) {
+    assert(err == null, err);
+    var data = [];
+    cursor.toArray(function(err, data) {
+      res.render('coordinator-dashboard', data);
+    });
+  });
 });
 
 module.exports = router;
