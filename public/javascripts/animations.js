9d3d4faef6bf524d05dfd549b30979807172ea0e
simke9445 <simke9445@gmail.com>
2016-03-05 03:59:02 +0100

    added slide bar

--- public/javascripts/animations.js
+++ public/javascripts/animations.js
@@ -2,22 +2,19 @@
 // progress bar animation
 $(document).ready(function() {
     $('.progress .progress-bar').progressbar({use_percentage: false, transition_delay: 900});
+    // Instantiate a slider
+    var mySlider = $("input.slider").slider();
+
+
+    $(".fa-map-marker, .fa-undo").click(function(){
+      $(this).closest(".card").toggleClass("flipped");
+      return false;
+    });
+
 });
 
-// Instantiate a slider
-var mySlider = $("input.slider").slider();
 
-// Call a method on the slider
-var value = mySlider.slider('getValue');
 
-// For non-getter methods, you can chain together commands
-    mySlider
-        .slider('setValue', 5)
-        .slider('setValue', 7);
 
-// card flip
 
-$(".fa-automobile, .fa-undo").click(function(){
-    $(this).closest(".card").toggleClass("flipped");
-    return false;
-});
+
