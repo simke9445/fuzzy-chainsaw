3418698857cc4fd6cc80fdd4f75e0f04724c1a44
Nikola Kolevski <nikola.z.kolevski@gmail.com>
2016-03-05 05:11:41 +0100

    Add posts.

---
+++ realtime/posts.js
@@ -0,0 +1,7 @@
+var r = require('rethinkdb');
+var assert = require('assert');
+
+module.exports = function(io, app) {
+    io.on('connection', function(socket) {
+    });
+};
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var assert = require('assert');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    r.db(app.locals.config.rethinkdb.db_name).table('event').run(app.locals.rdbConn, function(err, cursor) {
+        assert(err == null, err);
+        var data = [];
+        cursor.toArray(function(err, data) {
+            res.render('index', { title: 'Express', posts: data });
+        });
+    });
+}
