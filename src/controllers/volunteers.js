d0ec8281a0c246d676d9761e061a64d4a475dacf
Nikola Miljkovic <milja1337@gmail.com>
2016-03-30 15:03:37 +0200

    Added form logic to /signup controller, parts of validation are added, there should be a change in frontend to handle validation errors

--- src/controllers/volunteers.js
+++ src/controllers/volunteers.js
@@ -4,29 +4,75 @@ var io = require('socket.io');
 var ReactDOMServer = require('react-dom/server');
 var React = require('react');
 var VolunteerList = require('../../dist/component.bundle.js').VolunteerList;
-
+var r = require('rethinkdb');
 var localStore = null;
-    
+var db = null;
+
 /* GET volunteer listing. */
 router.get('/', function(req, res, next) {     
     var cardList = ReactDOMServer.renderToString(VolunteerList({
         cards: localStore.getState().entries
     }));
-   
+
     res.render('volunteer-list', {
-        mainList: cardList, 
+        mainList: cardList,
         state: JSON.stringify(localStore.getState().entries)
     });
 });
 
+var signupValidationSchema = {
+    'first_name': {
+        notEmpty: {
+            errorMessage: 'Polje ime je obavezno.'
+        }
+    },
+    'last_name': {
+        notEmpty: {
+            errorMessage: 'Polje prezime je obavezno.'
+        }
+    },
+    'phone': {
+        notEmpty: {
+            errorMessage: 'Polje mobilni telefon je obavezno.'
+        },
+        isPhoneNumber: {
+            errorMessage: 'Broj telefona je u losem formatu.'
+        }
+    },
+    'JMBG': {
+        isInt: true,
+        notEmpty: {
+            errorMessage: 'Polje JMBG je obavezno'
+        }
+    }
+};
+
 router.post('/signup', function(req, res, next) {
     console.log(req.body);
-
-    var cardList = ReactDOMServer.renderToString(VolunteerList({
-        cards: localStore.getState().entries
-    }));
-   
-    res.redirect('/');
+    req.checkBody(signupValidationSchema);
+    
+    var errors = req.validationErrors();
+    if (errors === false) {
+        var eventId = req.body.event_id;
+        delete req.body.event_id;
+        console.log(req.body);
+        r.table('event')
+            .get(eventId)
+            .update({
+                signups: r.row('signups').append(req.body),
+                available_manpower: r.row('signups').count()
+            })
+            .run(db);
+            
+        res.redirect('/');
+    } else {
+        res
+            .status(400)
+            .send(errors.map(function(err) {
+                return err.msg
+            })
+            .join(','));
+    }
 });
 
 router.post('/profile', function(req, res, next) {
@@ -35,8 +81,9 @@ router.post('/profile', function(req, res, next) {
     res.render('volunteer-detail');
 });
 
-module.exports = function(dataStore) {
+module.exports = function(dataStore, databaseRef) {
     localStore = dataStore;
-      
+    db = databaseRef;
+    
     return router;
 }
